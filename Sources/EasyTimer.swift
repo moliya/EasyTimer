89e002efc5f505e891a1a6bdacc143c829ecf356
carefree <946715806@qq.com>
2021-07-09 17:32:54 +0800

    fix protocol bug

--- Sources/EasyTimer.swift
+++ Sources/EasyTimer.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 @objc(KFEasyTimerUpdater)
-protocol EasyTimerUpdater: NSObjectProtocol {
+public protocol EasyTimerUpdater: NSObjectProtocol {
     @objc optional func timerUpdate(interval: TimeInterval)
 }
 
